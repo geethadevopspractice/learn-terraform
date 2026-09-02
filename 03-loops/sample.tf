b672f9dcb5c814754f9a459464d2f53898e0d1e2
Geethika <akulageethika6@gmail.com>
2026-09-02 18:56:43 +0530

    variables in the terraform -18

--- 03-loops/sample.tf
+++ 03-loops/sample.tf
@@ -5,3 +5,17 @@ resource "null_resource" "dummy" {
 output "numbers" {
   value = [for i in range(1, 11) : i]
 }
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+    count = 2
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "web"
+  }
+}
